fbd8386e49620461c9509388bb9753ca23508103
Hans Dorrestijn <hansie211@hotmail.com>
2022-11-26 21:20:50 +0100

    Seperate version file

--- src/stores/settings-store.js
+++ src/stores/settings-store.js
@@ -1,12 +1,13 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
+import version from "src/data/version.json";
 
 export const useSettingsStore = defineStore("settings", () => {
     const showErrors = ref(true);
     const showTime = ref(true);
     const showHints = ref(true);
 
-    const version = "v0.1";
+    const version = `v${version.major}.${version.minor}.${version.revision}`;
 
     return {
         showErrors,
