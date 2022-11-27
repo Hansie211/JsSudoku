be3496ce59c8659c3feb11a3b98c807cadd7a757
Hans <hansie211@hotmail.com>
2022-11-27 14:49:18 +0100

    Store settings

--- src/stores/settings-store.js
+++ src/stores/settings-store.js
@@ -1,18 +1,35 @@
 import { defineStore } from "pinia";
-import { ref } from "vue";
+import { ref, watch } from "vue";
 import versions from "src/data/version.json";
+import { SaveManager, Savestate } from "src/lib/savegame";
 
 export const useSettingsStore = defineStore("settings", () => {
-    const showErrors = ref(true);
-    const showTime = ref(true);
-    const showHints = ref(true);
+    const settings = {
+        showErrors: ref(undefined),
+        showTime: ref(undefined),
+        showHints: ref(undefined),
+    };
+
+    const saveManager = new SaveManager();
+
+    Object.keys(settings).forEach((key) => {
+        saveManager.states.push(
+            new Savestate(
+                key,
+                () => settings[key].value,
+                (v) => (settings[key].value = v),
+                () => true
+            )
+        );
+    });
+    saveManager.load();
+
+    Object.keys(settings).forEach((key) => watch(settings[key], () => saveManager.save()));
 
     const version = `v${versions.major}.${versions.minor}.${versions.rev}`;
 
     return {
-        showErrors,
-        showTime,
-        showHints,
+        ...settings,
         version,
     };
 });
