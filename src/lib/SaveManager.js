c12bb53585189b0bdc832ad381b3f81d4c5efa41
Hans <hansie211@hotmail.com>
2022-11-28 19:15:29 +0100

    Rename SaveManager

---
+++ src/lib/SaveManager.js
@@ -0,0 +1,69 @@
+import { LocalStorage } from "quasar";
+
+export class Savestate {
+    /** @type {() => State} */
+    getState;
+
+    /** @type {(State) => void} */
+    setState;
+
+    /** @type {() => void} */
+    setDefault;
+
+    /** @type {String} */
+    key;
+
+    /**
+     * @param {String} key
+     * @param {() => State} getState
+     * @param {(State) => void} setState
+     * @param {() => void} setDefault
+     */
+    constructor(key, getState, setState, setDefault) {
+        this.key = key;
+        this.getState = getState;
+        this.setState = setState;
+        this.setDefault = setDefault;
+    }
+}
+
+export default class SaveManager {
+    /** @type {Array<Savestate} */
+    states;
+
+    /** @type {String} */
+    storageKey;
+
+    save() {
+        const save = {};
+
+        this.states.forEach((state) => {
+            const key = state.key;
+            const savedState = state.getState();
+            save[key] = JSON.stringify(savedState);
+        });
+
+        LocalStorage.set(this.storageKey, save);
+    }
+
+    load() {
+        const save = LocalStorage.getItem(this.storageKey) ?? {};
+        this.states.forEach((state) => {
+            const key = state.key;
+            try {
+                const savedState = JSON.parse(save[key]);
+                state.setState(savedState);
+            } catch (error) {
+                state.setDefault();
+            }
+        });
+    }
+
+    /**
+     * @param {String} storageKey
+     */
+    constructor(storageKey) {
+        this.storageKey = storageKey;
+        this.states = [];
+    }
+}
