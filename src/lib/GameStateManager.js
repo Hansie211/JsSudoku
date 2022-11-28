55f7ee786accc2cfc9d0c5c2673f1d540357237e
Hans <hansie211@hotmail.com>
2022-11-28 19:16:31 +0100

    Create GameStateManager

---
+++ src/lib/GameStateManager.js
@@ -0,0 +1,274 @@
+import EventBus from "./eventbus";
+import Memory from "./Memory";
+import PuzzleBoard, { Cell } from "./reactiveSoduku";
+import SaveManager, { Savestate } from "./SaveManager";
+import Timer from "./Timer";
+import Difficulty from "src/data/difficulty.json";
+import { getPuzzle } from "./sudoku/sudoku";
+
+export default class GameStateManager {
+    /** @type {PuzzleBoard} */
+    puzzle;
+
+    /** @type {SaveManager} */
+    saveManager;
+
+    /** @type {Memory} */
+    memory;
+
+    /** @type {Timer} */
+    timer;
+
+    /** @type {Number} */
+    hintCount;
+
+    /** @type {EventBus} */
+    eventBus;
+
+    /**
+     * @param {GameStateManager} stateManager
+     */
+    static __initSaveManager(stateManager) {
+        stateManager.saveManager = new SaveManager("game");
+        stateManager.saveManager.states.push(
+            new Savestate(
+                "puzzle",
+                () => PuzzleBoard.serialize(stateManager.puzzle),
+                (state) => PuzzleBoard.deserialize(stateManager.puzzle, state),
+                () => {
+                    const [solution, board, seed] = getPuzzle(20);
+                    PuzzleBoard.fromBoard(stateManager.puzzle, board, solution, seed);
+                    stateManager.puzzle.difficultyLevel = 0;
+                }
+            ),
+            new Savestate(
+                "time",
+                () => stateManager.timer.time,
+                (state) => (stateManager.timer.time = state),
+                () => (stateManager.timer.time = 0)
+            ),
+            new Savestate(
+                "hintCount",
+                () => stateManager.hintCount,
+                (state) => (stateManager.hintCount = state),
+                () => (stateManager.hintCount = 0)
+            ),
+            new Savestate(
+                "memory",
+                () => Memory.serialize(stateManager.memory),
+                (state) => {
+                    Memory.deserialize(stateManager.memory, state);
+                },
+                () => stateManager.memory.clear()
+            )
+        );
+    }
+
+    /**
+     * @param {Number} cellId
+     * @param {Object} value
+     * @param {Boolean} isNote
+     * @param {Boolean} isHint
+     */
+    __createMemoryState(cellId, value, { isNote, isHint } = {}) {
+        return {
+            cellId,
+            value,
+            isNote: isNote === true,
+            isHint: isHint === true,
+        };
+    }
+
+    /**
+     * @param {Number} cellId
+     * @param {Object} value
+     * @param {Boolean} isNote
+     * @param {Boolean} isHint
+     */
+    __saveMemoryState(cellId, value, { isNote, isHint } = {}) {
+        const state = this.__createMemoryState(cellId, value, { isNote, isHint });
+        this.memory.store(state);
+    }
+
+    __updateVictoryState() {
+        const isVictory = this.puzzle.cells.reduce((state, cell, idx) => state && this.puzzle.solution[idx] === cell.value, true);
+        if (!isVictory) return false;
+
+        this.eventBus.dispatch("victory", {});
+        return true;
+    }
+
+    /** @param {Cell} */
+    __ensureCanEditCell(cell) {
+        if (!cell) {
+            this.__dispatchError("No cell selected.");
+            return false;
+        }
+
+        if (cell.isStatic) {
+            this.__dispatchError("Cannot edit this cell.");
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @param {String} errorMessage
+     */
+    __dispatchError(errorMessage) {
+        this.eventBus.dispatch("error", { message: errorMessage });
+    }
+
+    /**
+     * @param {Number} cellId
+     * @param {Number} value
+     * @param {Boolean} isHint
+     */
+    placeNum(cellId, value, { isHint = false } = {}) {
+        const cell = this.puzzle.findCellById(cellId);
+        if (!this.__ensureCanEditCell(cell)) return;
+
+        this.__saveMemoryState(cellId, cell.value, { isNote: false, isHint });
+
+        if (cell.value === value) {
+            cell.value = 0;
+        } else {
+            cell.value = value;
+            if (this.__updateVictoryState()) return;
+        }
+
+        this.saveManager.save();
+    }
+
+    /**
+     * @param {Number} cellId
+     * @param {Number} value
+     */
+    placeNote(cellId, value) {
+        const cell = this.puzzle.findCellById(cellId);
+        if (!this.__ensureCanEditCell(cell)) return;
+        if (cell.hasValue()) {
+            this.__dispatchError("Cell already has a value.");
+            return;
+        }
+
+        this.__saveMemoryState(cellId, cell.notes.values, { isNote: true, isHint: false });
+
+        cell.notes.swapValue(value);
+
+        this.saveManager.save();
+    }
+
+    /**
+     * @param {Number} cellId
+     */
+    clearCell(cellId) {
+        const cell = this.puzzle.findCellById(cellId);
+        if (!this.__ensureCanEditCell(cell)) return;
+
+        if (!cell.hasValue() && !cell.notes.values.length) return;
+
+        if (cell.hasValue()) {
+            this.__saveMemoryState(cellId, cell.value, { isNote: false, isHint: false });
+            cell.value = 0;
+        } else {
+            this.__saveMemoryState(cellId, cell.notes.values, { isNote: true, isHint: false });
+            cell.notes.clear();
+        }
+
+        this.saveManager.save();
+    }
+
+    undo() {
+        if (!this.memory.size) return;
+
+        const states = this.memory.rollBack();
+
+        states.forEach((state) => {
+            const cell = this.puzzle.findCellById(state.cellId);
+            const value = state.value;
+            switch (state.isNote) {
+                case false: {
+                    cell.value = value;
+                    break;
+                }
+                case true: {
+                    cell.notes.set(value);
+                    break;
+                }
+            }
+
+            if (state.isHint) this.hintCount--;
+        });
+
+        this.saveManager.save();
+    }
+
+    hint() {
+        const nextOpenCell = this.puzzle.cells.find((cell) => !cell.hasValue());
+        if (!nextOpenCell) return;
+
+        const soltionValue = this.puzzle.solution[PuzzleBoard.toIndex(nextOpenCell.position)];
+
+        this.__saveMemoryState(nextOpenCell.id, 0, { isNote: false, isHint: true });
+
+        nextOpenCell.value = soltionValue;
+        this.hintCount++;
+
+        if (this.__updateVictoryState()) return;
+
+        this.saveManager.save();
+    }
+
+    resetGameState() {
+        this.timer.set(0);
+        this.memory.clear();
+        this.hintCount = 0;
+
+        this.saveManager.save();
+    }
+
+    resetLevel() {
+        this.puzzle.cells.forEach((cell) => {
+            if (cell.isStatic) return;
+            cell.value = 0;
+            cell.notes.clear();
+        });
+
+        this.resetGameState();
+        this.saveManager.save();
+    }
+
+    /**
+     * @param {Number} difficultyLevel
+     * @param {Number} seed
+     * @returns {Promise<void>}
+     */
+    async newLevel(difficultyLevel, seed) {
+        const difficulty = Difficulty.find((item) => item.level === difficultyLevel) ?? Difficulty[0];
+
+        return new Promise((resolve) => {
+            window.setTimeout(() => {
+                const [solution, board, _seed] = getPuzzle(difficulty.cells, seed);
+                PuzzleBoard.fromBoard(this.puzzle, board, solution, _seed);
+                this.puzzle.difficultyLevel = difficultyLevel;
+
+                this.resetGameState();
+                this.saveManager.save();
+
+                resolve();
+            }, 100);
+        });
+    }
+
+    constructor() {
+        this.eventBus = new EventBus();
+        this.memory = new Memory();
+        this.timer = new Timer();
+        this.puzzle = new PuzzleBoard();
+        this.hintCount = 0;
+
+        GameStateManager.__initSaveManager(this);
+    }
+}
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,28 +1,18 @@
 <template>
-    <q-page id="page" v-if="puzzle">
+    <q-page id="page" v-if="gameState.puzzle">
         <div style="padding: 10px">
-            <sudoku-board ref="sudoku" :puzzle="puzzle" :key="renderKey" size="4.8vw" @cellSelected="cellSelected" />
+            <sudoku-board ref="sudoku" :puzzle="gameState.puzzle" :key="renderKey" size="4.8vw" @cellSelected="cellSelected" />
         </div>
         <div id="bottom-bar" class="flex column full-width">
             <div id="info-bar" class="flex row full-width q-px-sm">
-                <timer ref="timer" :initialValue="initalTime" v-show="settings.showTime" class="text-caption flex-center" />
+                <div>{{ gameState.timer.time }}</div>
                 <q-space />
-                <div class="text-caption q-mr-sm flex flex-center"><q-icon v-for="x in puzzle.difficultyLevel + 1" :key="x" name="star" color="yellow" /></div>
-                <div class="text-caption flex-center">Hints {{ hintCount }}</div>
+                <div class="text-caption q-mr-sm flex flex-center"><q-icon v-for="x in gameState.puzzle.difficultyLevel ?? 0 + 1" :key="x" name="star" color="yellow" /></div>
+                <div class="text-caption flex-center">Hints {{ gameState.hintCount }}</div>
             </div>
             <div id="action-bar" class="full-width">
                 <div id="mode-bar" class="row q-pa-sm">
-                    <q-btn
-                        icon="settings"
-                        round
-                        flat
-                        size="md"
-                        @click="
-                            () => {
-                                showSettings = true;
-                            }
-                        "
-                    />
+                    <q-btn icon="settings" round flat size="md" @click="showSettings" />
                     <q-space />
                     <q-btn icon="undo" flat round size="md" @click="undo" />
                     <q-btn icon="lightbulb" flat round size="md" @click="hint" v-show="settings.showHints" />
@@ -33,87 +23,52 @@
                 <number-bar :size="9" @click="placeNumber" :activeNumbers="activeNumbers" />
             </div>
         </div>
-        <settings-dialog v-model="showSettings" :puzzle="puzzle" @resetGame="resetGame" @newLevel="newLevel" />
+        {{ demo.deep.value }}
+        <!-- <settings-dialog v-model="showSettings" :puzzle="gameState.puzzle" @resetGame="resetGame" @newLevel="newLevel" /> -->
     </q-page>
 </template>
 
 <script>
 import SudokuBoard from "src/components/SudokuBoard.vue";
-import SettingsDialog from "src/components/SettingsDialog.vue";
+import SettingsScreen from "src/components/SettingsScreen";
 import { StructureDefinitions } from "src/lib/sudoku/board";
-import { getPuzzle } from "src/lib/sudoku/sudoku";
-import { defineComponent, reactive, ref } from "vue";
-import PuzzleBoard, { Cell } from "src/lib/reactiveSoduku";
-import Timer from "src/components/Timer.vue";
+import { defineComponent, reactive, ref, toRef } from "vue";
+import { Cell } from "src/lib/reactiveSoduku";
 import { useSettingsStore } from "src/stores/settings-store";
-import NumberBar from "src/components/NumberBar.vue";
-import Memory from "src/lib/memory";
-import VictoryScreen from "src/components/VictoryScreen.vue";
-import { SaveManager, Savestate } from "src/lib/savegame";
+import NumberBar from "src/components/NumberBar";
+import VictoryScreen from "src/components/VictoryScreen";
+import GameStateManager from "src/lib/GameStateManager";
 
 export default defineComponent({
-    components: { SudokuBoard, SettingsDialog, Timer, NumberBar },
+    components: { SudokuBoard, NumberBar },
     name: "IndexPage",
     setup() {
         const settings = useSettingsStore();
-        const saveManager = new SaveManager("game");
+        const gameState = reactive(new GameStateManager());
 
         return {
             settings,
-            saveManager,
+            /** @type {GameStateManager} */
+            gameState,
             renderKey: ref(0),
-            /** @type {PuzzleBoard} */
-            puzzle: reactive(null),
-            initalTime: ref(0),
-            hintCount: ref(0),
-            /** @type {Memory} */
-            memory: null,
+            demo,
         };
     },
     created() {
-        this.saveManager.states.push(
-            new Savestate(
-                "puzzle",
-                () => this.puzzle,
-                (v) => (this.puzzle = v),
-                () => {
-                    const [solution, board, seed] = getPuzzle(20);
-                    const puzzle = PuzzleBoard.fromBoard(board, solution, seed);
-                    puzzle.difficultyLevel = 0;
-                    return puzzle;
-                },
-                PuzzleBoard.serialize,
-                PuzzleBoard.deserialize
-            ),
-            new Savestate(
-                "time",
-                () => this.initalTime,
-                (v) => (this.initalTime = v),
-                () => 0
-            ),
-            new Savestate(
-                "hintCount",
-                () => this.hintCount,
-                (v) => (this.hintCount = v),
-                () => 0
-            ),
-            new Savestate(
-                "memory",
-                () => this.memory,
-                (v) => (this.memory = v),
-                () => new Memory(),
-                Memory.serialize,
-                Memory.deserialize
-            )
-        );
+        this.gameState.saveManager.load();
 
-        this.saveManager.load();
+        this.gameState.eventBus.on("victory", (event) => {
+            this.showVictory();
+        });
+
+        this.gameState.eventBus.on("error", (event) => {
+            this.showError(event.message);
+        });
     },
     data() {
         return {
             noteMode: false,
             selectedCellId: null,
-            showSettings: false,
         };
     },
     methods: {
@@ -126,37 +81,15 @@ export default defineComponent({
                 actions: [{ icon: "close", color: "white" }],
             });
         },
-        canEditSelectedCell() {
-            if (!this.selectedCell) {
-                this.showError("No cell selected.");
-                return false;
-            }
-
-            if (this.selectedCell.isStatic) {
-                this.showError("Cannot edit this cell.");
-                return false;
-            }
-
-            return true;
-        },
-
-        saveGameState() {
-            this.saveManager.save();
-        },
-        isVictory() {
-            return this.puzzle.cells.reduce((state, cell, idx) => state && this.puzzle.solution[idx] === cell.value, true);
-        },
         showVictory() {
-            if (!this.isVictory()) return;
-
-            const time = this.$refs.timer.getTime();
+            const time = this.gameState.timer.time;
 
             const victories = this.$q.localStorage.getItem("victories")?.data ?? [];
             victories.push({
-                level: this.puzzle.seed,
+                level: this.gameState.puzzle.seed,
                 time: time,
-                hintCount: this.hintCount,
-                difficultyLevel: this.puzzle.difficultyLevel,
+                hintCount: this.gameState.hintCount,
+                difficultyLevel: this.gameState.puzzle.difficultyLevel,
             });
             this.$q.localStorage.set("victories", { data: victories });
 
@@ -164,177 +97,60 @@ export default defineComponent({
                 .dialog({
                     component: VictoryScreen,
                     componentProps: {
-                        puzzle: this.puzzle,
-                        time: this.$refs.timer.getTime(),
-                        hintCount: this.hintCount,
+                        puzzle: this.gameState.puzzle,
+                        time: time,
+                        hintCount: this.gameState.hintCount,
                     },
                 })
-                .onOk(this.newLevel);
+                .onOk((info) => this.gameState.newLevel(info.level, info.seed));
         },
-        /**
-         * @param {Number} cellId
-         * @param {Number} value
-         * @param {Boolean} isNote
-         * @param {Boolean} isHint
-         * @returns {Object}
-         */
-        createState(cellId, value, isNote, isHint) {
-            return {
-                cellId,
-                value,
-                isNote: isNote === true,
-                isHint: isHint === true,
-            };
-        },
-
-        initNewGame() {
-            this.$refs.timer.reset();
-            this.memory.clear();
-            this.hintCount = 0;
-
-            this.saveGameState();
-        },
-        resetGame() {
-            this.puzzle.cells.forEach((cell) => {
-                if (cell.isStatic) return;
-                cell.value = 0;
-                cell.notes.values.length = 0;
-            });
-
-            this.initNewGame();
-        },
-        newLevel(levelInfo) {
-            const emptySquares = {
-                0: 16,
-                1: 32,
-                2: 48,
-                3: 64,
-            };
-
-            const squareCount = emptySquares[levelInfo.level.toString()];
-
-            this.$q.loading.show({
-                message: "Generating level...",
-                delay: 1,
-            });
-
-            window.setTimeout(() => {
-                const [solution, board, seed] = getPuzzle(squareCount, levelInfo.seed);
-                this.puzzle = PuzzleBoard.fromBoard(board, solution, seed);
-                this.puzzle.difficultyLevel = levelInfo.level;
-                this.renderKey++;
-                this.$q.loading.hide();
-
-                this.initNewGame();
-            }, 100);
+        showSettings() {
+            this.$q
+                .dialog({
+                    component: SettingsScreen,
+                    componentProps: {
+                        currentLevel: this.gameState.puzzle.seed,
+                    },
+                })
+                .onOk((action) => {
+                    switch (action.name) {
+                        case "new-level":
+                            this.gameState.newLevel(action.data.level, action.data.seed);
+                            break;
+                        case "reset-level":
+                            this.gameState.resetLevel();
+                            break;
+                    }
+                });
         },
 
         placeNumber(num) {
-            if (!this.canEditSelectedCell()) return;
+            this.demo.deep.value++;
 
-            if (!this.noteMode) {
-                const state = this.createState(this.selectedCellId, this.selectedCell.value, false, false);
-                this.memory.store(state);
-
-                if (this.selectedCell.value === num) {
-                    this.selectedCell.value = 0;
-                } else {
-                    this.selectedCell.value = num;
-                    this.showVictory();
-                }
-
-                this.saveGameState();
-            } else {
-                if (this.selectedCell.hasValue()) {
-                    this.showError("Cell already filled selected.");
-                } else {
-                    const state = this.createState(this.selectedCellId, this.selectedCell.notes.values, true, false);
-                    this.memory.store(state);
-
-                    this.selectedCell.notes.swapValue(num);
-
-                    this.saveGameState();
-                }
-            }
+            if (this.noteMode) this.gameState.placeNote(this.selectedCellId, num);
+            else this.gameState.placeNum(this.selectedCellId, num);
         },
+
         clearCell() {
-            if (!this.canEditSelectedCell()) return;
-
-            const clearNotes = !this.selectedCell.hasValue();
-            if (clearNotes && !this.selectedCell.notes.values.length) return;
-
-            switch (clearNotes) {
-                case true: {
-                    const state = this.createState(this.selectedCellId, this.selectedCell.notes.values, true, false);
-                    this.memory.store(state);
-
-                    this.selectedCell.notes.clear();
-                    break;
-                }
-                case false: {
-                    const state = this.createState(this.selectedCellId, this.selectedCell.value, false, false);
-                    this.memory.store(state);
-
-                    this.selectedCell.value = 0;
-                }
-            }
-
-            this.saveGameState();
+            this.gameState.clearCell(this.selectedCellId);
         },
         hint() {
-            const nextOpenCell = this.puzzle.cells.find((cell) => !cell.value);
-            if (!nextOpenCell) return;
-
-            const soltionValue = this.puzzle.solution[PuzzleBoard.toIndex(nextOpenCell.position)];
-
-            const state = this.createState(nextOpenCell.id, 0, false, true);
-            this.memory.store(state);
-
-            nextOpenCell.value = soltionValue;
-            this.hintCount++;
-
-            this.showVictory();
-
-            this.saveGameState();
+            this.gameState.hint();
         },
         cellSelected(id) {
             this.selectedCellId = id;
         },
         undo() {
-            if (!this.memory.size) return;
-
-            const states = this.memory.rollBack();
-
-            states.forEach((state) => {
-                const cell = this.puzzle.findCellById(state.cellId);
-                const value = state.value;
-                switch (state.isNote) {
-                    case false: {
-                        cell.value = value;
-                        break;
-                    }
-                    case true: {
-                        cell.notes.set(value);
-                        break;
-                    }
-                }
-
-                if (state.isHint) this.hintCount--;
-            });
-
-            this.saveGameState();
+            this.gameState.undo();
         },
     },
     computed: {
         boardSize() {
             return StructureDefinitions.SIZE;
         },
-        // selectedCellIndex() {
-        //     return -1; //this.puzzle.cells.findIndex((x) => x.id === this.selectedCellId);
-        // },
         /** @returns {Cell} */
         selectedCell() {
-            return this.puzzle.findCellById(this.selectedCellId);
+            return this.gameState.puzzle.findCellById(this.selectedCellId);
         },
         activeNumbers() {
             if (!this.selectedCell) return [];
@@ -342,6 +158,16 @@ export default defineComponent({
             if (this.selectedCell.value) return [];
 
             return this.selectedCell.notes.values;
+        },
+        gameTime() {
+            return toRef(this.gameState, "timer");
+        },
+        puzzleTime() {
+            const time = this.gameState.timer.time;
+            const s = 0 + (time % 60);
+            const m = 0 + Math.floor(time / 60);
+
+            return m.toString().padStart(2, "0") + ":" + s.toString().padStart(2, "0");
         },
     },
 });
