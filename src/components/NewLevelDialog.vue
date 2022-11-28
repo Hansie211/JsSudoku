f9663f6bce0bbfc494e1fab757a36bd795a6420a
Hans Dorrestijn <hansie211@hotmail.com>
2022-11-28 20:11:22 +0100

    Use difficulty.json

--- src/components/NewLevelDialog.vue
+++ src/components/NewLevelDialog.vue
@@ -7,7 +7,9 @@
             </q-card-section>
             <q-card-section class="text-h6"> Niveau </q-card-section>
             <q-card-section class="q-gutter-y-md">
-                <q-btn v-for="level in difficultyLevels" :key="level.level" color="primary" outline size="lg" align="left" :label="level.name" class="full-width" @click="() => onOKClick(level.level)"><q-space /><q-icon v-for="x in level.level + 1" :key="x" name="star" color="yellow" /></q-btn>
+                <q-btn v-for="difficulty in Difficulty" :key="difficulty.level" color="primary" outline size="lg" align="left" :label="difficulty.name" class="full-width" @click="() => onOKClick(difficulty)"
+                    ><q-space /><q-icon v-for="x in difficulty.level + 1" :key="x" name="star" color="yellow"
+                /></q-btn>
             </q-card-section>
             <q-card-section>
                 <q-btn label="Cancel" class="full-width" color="negative" @click="onCancelClick" />
@@ -18,30 +20,14 @@
 
 <script>
 import { ref } from "vue";
+import Difficulty from "src/data/difficulty.json";
 export default {
     name: "NewLevelDialog",
     props: {},
     emits: ["ok", "hide"],
     setup() {
         return {
-            difficultyLevels: [
-                {
-                    name: "Easy",
-                    level: 0,
-                },
-                {
-                    name: "Medium",
-                    level: 1,
-                },
-                {
-                    name: "Hard",
-                    level: 2,
-                },
-                {
-                    name: "Extra Hard",
-                    level: 3,
-                },
-            ],
+            Difficulty,
             levelNumber: ref(Math.floor(Math.random() * 4294967296)),
         };
     },
